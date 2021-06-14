d6e2c7beddbbea1215a0d1dc4c9ac6fe618857c8
Cooper Saye <coopersaye@gmail.com>
2021-06-14 00:30:29 -0700

    Move head to _app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="App">
+      <Head>
+        <title>Shiftmate</title>
+        <meta name="description" content="Company scheduling and communication." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      <footer></footer>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
