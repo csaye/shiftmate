c135097d23547c7b8ed786a0eb0eba0ed0795581
Cooper Saye <coopersaye@gmail.com>
2021-06-21 23:16:51 -0700

    Initialize firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,16 @@
 import Calendar from '../Calendar/Calendar.js';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+
 import './App.css';
+
+// initialize firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 function App() {
   return (
