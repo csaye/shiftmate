aa91d866d1ed8ced398fd059ac97e987f6de4cb1
Cooper Saye <coopersaye@gmail.com>
2021-06-22 23:04:09 -0700

    Implement event creation.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -5,13 +6,31 @@ import interactionPlugin from '@fullcalendar/interaction';
 import './Calendar.css';
 
 function Calendar() {
+  const [events, setEvents] = useState([]);
+
+  // creates and appends new event
+  function createEvent(start, end) {
+    const newEvent = {
+      start: start.getTime(),
+      end: end.getTime(),
+      title: 'Event'
+    };
+    // append event
+    setEvents(oldEvents => [...oldEvents, newEvent]);
+  }
+
   return (
     <FullCalendar
+      events={events}
       plugins={[timeGridPlugin, interactionPlugin]}
       allDaySlot={false}
       slotMinTime="06:00:00"
       slotMaxTime="22:00:00"
       nowIndicator={true}
+      selectable={true}
+      select={selectInfo => {
+        createEvent(selectInfo.start, selectInfo.end);
+      }}
     />
   );
 }
