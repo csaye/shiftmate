fdf3fd0edd229b2e83dce285d0a50206f59b1d59
Cooper Saye <coopersaye@gmail.com>
2021-06-24 10:07:50 -0700

    Improve header styling.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 import firebase from 'firebase/app';
 
 import './Header.css';
+import logo from '../../img/logo.png';
 
 function Header() {
   return (
     <div className="Header">
-      <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
+      <h1>Shiftmate</h1>
+      <img src={logo} alt="logo" />
+      <span className="flex-fill" />
+      <button
+        className="icon-button"
+        onClick={() => firebase.auth().signOut()}
+      >
+        <ExitToAppIcon />
+      </button>
     </div>
   );
 }
