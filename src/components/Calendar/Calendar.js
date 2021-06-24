720ce83eab8fef27f93565e6a26fae256af33f22
Cooper Saye <coopersaye@gmail.com>
2021-06-23 19:08:09 -0700

    Integrate events with firebase.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
+
+import firebase from 'firebase/app';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import './Calendar.css';
 
 function Calendar() {
-  const [events, setEvents] = useState([]);
+  const uid = firebase.auth().currentUser.uid;
+  const eventsCol = firebase.firestore().collection('events');
+  const [eventsData] = useCollectionData(eventsCol, { idField: 'id' });
 
-  // creates and appends new event
-  function createEvent(start, end) {
-    const newEvent = {
-      id: Date.now().toString(),
-      start: start.getTime(),
-      end: end.getTime(),
-      title: 'Event'
-    };
-    // append event
-    setEvents(oldEvents => [...oldEvents, newEvent]);
+  // turns given selection into firebase event
+  async function createEvent(selectInfo) {
+    await eventsCol.add({
+      start: selectInfo.start.getTime(),
+      end: selectInfo.end.getTime(),
+      title: 'Event',
+      creator: uid
+    });
+  }
+
+  // deletes given event in firebase
+  async function deleteEvent(eventInfo) {
+    if (!window.confirm(`Delete ${eventInfo.event.title}?`)) return;
+    await eventsCol.doc(eventInfo.event.id).delete();
   }
 
   return (
     <FullCalendar
-      events={events}
+      events={eventsData}
       plugins={[timeGridPlugin, interactionPlugin]}
       allDaySlot={false}
       slotMinTime="06:00:00"
       slotMaxTime="22:00:00"
       nowIndicator={true}
       selectable={true}
-      select={selectInfo => {
-        createEvent(selectInfo.start, selectInfo.end);
-      }}
-      eventClick={eventInfo => {
-        if (!window.confirm(`Delete ${eventInfo.event.title}?`)) return;
-        const id = eventInfo.event.id;
-        setEvents(oldEvents => oldEvents.filter(e => e.id !== id));
-      }}
+      select={createEvent}
+      eventClick={deleteEvent}
     />
   );
 }
