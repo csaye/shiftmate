387447a3a72450ecfc4fb32dfc76eb032ffcb5df
Cooper Saye <coopersaye@gmail.com>
2021-06-24 13:04:24 -0700

    Retrieve user data.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,13 @@ import firebase from 'firebase/app';
 import './Header.css';
 import logo from '../../img/logo.png';
 
-function Header() {
+function Header(props) {
   return (
     <div className="Header">
       <h1>Shiftmate</h1>
       <img src={logo} alt="logo" />
       <span className="flex-fill" />
+      <p>Signed in as {props.userData.name}</p>
       <button
         className="icon-button"
         onClick={() => firebase.auth().signOut()}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,50 @@
+import React, { useEffect, useState } from 'react';
 import Calendar from '../Calendar/Calendar.js';
 import Header from '../Header/Header.js';
+
+import firebase from 'firebase/app';
 
 import './Home.css';
 
 function Home() {
+  const [userData, setUserData] = useState(undefined);
+
+  const user = firebase.auth().currentUser;
+  const uid = user.uid;
+  const userDoc = firebase.firestore().collection('users').doc(uid);
+
+  // retrieves user data from firebase and creates it if none
+  async function getUserData() {
+    const doc = await userDoc.get();
+    // if user doc exists, set data
+    if (doc.exists) setUserData(doc.data());
+    // if user doc does not exist, create it
+    else {
+      const data = {
+        name: user.displayName,
+        profile: user.photoURL,
+        admin: false,
+        joined: new Date()
+      };
+      // update data
+      await userDoc.set(data);
+      setUserData(data);
+    }
+  }
+
+  // get user data on start
+  useEffect(() => {
+    getUserData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // if no user data, return loading
+  if (!userData) {
+    return <h1>Loading user...</h1>;
+  }
+
   return (
     <div className="Home">
-      <Header />
+      <Header userData={userData} />
       <Calendar />
     </div>
   );
