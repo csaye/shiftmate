39dda08c6e0758bb5a7fa515a2986a3204daa6ad
Cooper Saye <coopersaye@gmail.com>
2021-06-24 10:53:21 -0700

    Improve sign in styling.

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/app';
 
 import './SignIn.css';
+import logo from '../../img/logo.png';
+import google from '../../img/google.svg';
 
 function SignIn() {
   // opens google sign in popup
@@ -12,8 +14,14 @@ function SignIn() {
   return (
     <div className="SignIn">
       <div className="center-box">
+        <img className="logo" src={logo} alt="logo" />
         <h1>Shiftmate</h1>
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <button onClick={signInWithGoogle}>
+          <div className="left-side">
+            <img src={google} alt="google" />
+          </div>
+          <p>Sign in with Google</p>
+        </button>
       </div>
     </div>
   );
