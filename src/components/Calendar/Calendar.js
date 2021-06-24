5645f8b017ac0cb4d7b85bb0f0c84053783fda80
Cooper Saye <coopersaye@gmail.com>
2021-06-23 21:38:08 -0700

    Implement event title editing.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -11,6 +11,7 @@ import './Calendar.css';
 
 function Calendar() {
   const [editingEvent, setEditingEvent] = useState(null);
+  const [title, setTitle] = useState('');
 
   const uid = firebase.auth().currentUser.uid;
   const eventsCol = firebase.firestore().collection('events');
@@ -21,7 +22,7 @@ function Calendar() {
     await eventsCol.add({
       start: selectInfo.start.getTime(),
       end: selectInfo.end.getTime(),
-      title: 'Event',
+      title: 'New Event',
       creator: uid
     });
   }
@@ -29,11 +30,20 @@ function Calendar() {
   // deletes given event in firebase
   async function deleteEvent(eventInfo) {
     if (!window.confirm(`Delete ${eventInfo.event.title}?`)) return;
+    setEditingEvent(null);
     await eventsCol.doc(eventInfo.event.id).delete();
   }
 
   // updates given event in firebase
   async function updateEvent(eventInfo) {
+    setEditingEvent(null);
+    await eventsCol.doc(eventInfo.event.id).update({
+      title: title
+    });
+  }
+
+  // updates given event in firebase
+  async function updateEventTime(eventInfo) {
     await eventsCol.doc(eventInfo.event.id).update({
       start: eventInfo.event.start.getTime(),
       end: eventInfo.event.end.getTime()
@@ -58,12 +68,15 @@ function Calendar() {
         eventResizableFromStart={true}
         select={createEvent}
         eventClick={setEditingEvent}
-        eventDrop={updateEvent}
-        eventResize={updateEvent}
+        eventDrop={updateEventTime}
+        eventResize={updateEventTime}
       />
       <Modal
         isOpen={editingEvent ? true : false}
-        onRequestClose={() => setEditingEvent(false)}
+        onAfterOpen={() => {
+          setTitle(editingEvent?.event?.title);
+        }}
+        onRequestClose={() => setEditingEvent(null)}
         ariaHideApp={false}
         style={{
           content: {
@@ -79,8 +92,24 @@ function Calendar() {
         }}
       >
         <h1>Editing {editingEvent?.event?.title}</h1>
-        <button onClick={() => setEditingEvent(null)}>Cancel</button>
-        <button onClick={() => setEditingEvent(null)}>Save</button>
+        <button onClick={() => deleteEvent(editingEvent)}>Delete</button>
+        <form onSubmit={e => {
+          e.preventDefault();
+          updateEvent(editingEvent);
+        }}>
+          <input
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            required
+          />
+          <button
+            type="button"
+            onClick={() => setEditingEvent(null)}
+          >
+            Cancel
+          </button>
+          <button>Save</button>
+        </form>
       </Modal>
     </div>
   );
