c1f140cd65bc9b1aea9e45368f84d8e4f3e9de94
Cooper Saye <coopersaye@gmail.com>
2021-06-23 21:02:07 -0700

    Create edit event modal.

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,4 +1,6 @@
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
+import Modal from 'react-modal';
 
 import firebase from 'firebase/app';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -8,6 +10,8 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 import './Calendar.css';
 
 function Calendar() {
+  const [editingEvent, setEditingEvent] = useState(null);
+
   const uid = firebase.auth().currentUser.uid;
   const eventsCol = firebase.firestore().collection('events');
   const [eventsData] = useCollectionData(eventsCol, { idField: 'id' });
@@ -41,21 +45,44 @@ function Calendar() {
   }
 
   return (
-    <FullCalendar
-      events={eventsData}
-      plugins={[timeGridPlugin, interactionPlugin]}
-      allDaySlot={false}
-      slotMinTime="06:00:00"
-      slotMaxTime="22:00:00"
-      nowIndicator={true}
-      selectable={true}
-      editable={true}
-      eventResizableFromStart={true}
-      select={createEvent}
-      eventClick={deleteEvent}
-      eventDrop={updateEvent}
-      eventResize={updateEvent}
-    />
+    <div className="Calendar">
+      <FullCalendar
+        events={eventsData}
+        plugins={[timeGridPlugin, interactionPlugin]}
+        allDaySlot={false}
+        slotMinTime="06:00:00"
+        slotMaxTime="22:00:00"
+        nowIndicator={true}
+        selectable={true}
+        editable={true}
+        eventResizableFromStart={true}
+        select={createEvent}
+        eventClick={setEditingEvent}
+        eventDrop={updateEvent}
+        eventResize={updateEvent}
+      />
+      <Modal
+        isOpen={editingEvent ? true : false}
+        onRequestClose={() => setEditingEvent(false)}
+        ariaHideApp={false}
+        style={{
+          content: {
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '400px'
+          },
+          overlay: {
+            zIndex: '1'
+          }
+        }}
+      >
+        <h1>Editing {editingEvent?.event?.title}</h1>
+        <button onClick={() => setEditingEvent(null)}>Cancel</button>
+        <button onClick={() => setEditingEvent(null)}>Save</button>
+      </Modal>
+    </div>
   );
 }
 
